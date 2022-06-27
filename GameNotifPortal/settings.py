11ffbf8891691c9bc9229054a61c58c607f1c667
IvanDamNation <ivan.suvalkin@gmail.com>
2022-06-27 20:52:59 +0300

    Basic configuration in settings.py

--- GameNotifPortal/settings.py
+++ GameNotifPortal/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'GameNotifPortal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'GameNotifPortal/static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -136,4 +143,4 @@ LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_UNIQUE = True
 ACCOUNT_EMAIL_CONFIRMATION_REQUIRED = True
 
-#  LOGGING = log_configuration
+# LOGGING = log_configuration
