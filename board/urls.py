54d8c9bde9bb94894ce40dd1d7a6351281adbcff
IvanDamNation <ivan.suvalkin@gmail.com>
2022-06-27 21:17:43 +0300

    Very basic of urls. Marked 'TODO' for self

---
+++ board/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import NewsFeed, NewsByCategory, ViewNews, CreateNews
+
+
+# TODO
+urlpatterns = [
+
+]
--- board/views.py
+++ board/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
+from .models import News, Category
+
+
+# TODO
+class NewsFeed(ListView):
+    pass
+
+
+# TODO
+class NewsByCategory(ListView):
+    pass
+
+
+# TODO
+class ViewNews(DetailView):
+    pass
+
+
+# TODO
+class CreateNews(CreateView):
+    pass
