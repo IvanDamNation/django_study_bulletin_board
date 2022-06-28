163e3c6312780897e33fd2599073422802c3867d
IvanDamNation <ivan.suvalkin@gmail.com>
2022-06-28 20:52:33 +0300

    Add basic bootstrap, some templates for homepage and first view for news list

--- board/urls.py
+++ board/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+
 from .views import NewsFeed, NewsByCategory, ViewNews, CreateNews
 
 
 # TODO
 urlpatterns = [
+    path('', NewsFeed.as_view(), name='home'),
 
 ]
--- board/views.py
+++ board/views.py
@@ -6,7 +6,14 @@ from .models import News, Category
 
 # TODO
 class NewsFeed(ListView):
-    pass
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Main page'
+        return context
 
 
 # TODO
