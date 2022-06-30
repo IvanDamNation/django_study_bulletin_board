66e7880f7451ce9a01da4ffad49a7415966c9ed2
IvanDamNation <ivan.suvalkin@gmail.com>
2022-06-30 22:23:06 +0300

    Add register in templates and in admin panel

--- usersaccounts/views.py
+++ usersaccounts/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
 from django.views import View
+
+from usersaccounts.forms import UserCreationForm
 
 
 class Register(View):
@@ -20,7 +20,7 @@ class Register(View):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('home')
